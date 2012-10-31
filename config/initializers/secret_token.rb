5af1fe42363a69dade5e71cd1ba4217ff949f663
YoshifumiSato <yoshifumi0521@gmail.com>
2012-10-31 21:25:07 +0900

    Railsアプリを追加

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GoogleCalendarEventApp::Application.config.secret_token = '6a85f9b231038f37db9851ef116fa2f6799763e955505fa83caea396ba503d5b39d2951656baf1bdce3f88fc5e82792fda72745fe6225b7fa86958035d895ef7'
